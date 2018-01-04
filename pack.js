a2183c4fa2aca5ce4eedcf2bd4dc39225f96dfbc
w2moon <w2moon@gmail.com>
2018-01-04 15:13:44 +0800

    new save server

--- pack.js
+++ pack.js
@@ -1,7 +1,8 @@
 var wl = require("./tools/wl")
 var PATHS = [
    "../lovigame_ejs",
-   "../lovigame_admin" 
+   "../lovigame_admin",
+   "../enochsrc/wltool/saveserver"
 ];
 
 for(var i=0;i<PATHS.length;++i){
